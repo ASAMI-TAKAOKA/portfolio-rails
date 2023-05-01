26bc021e7e80860f43c42caf38595fcac17764f8
Asami Takaoka <asamiyamabe61@gmail.com>
2023-05-01 17:20:10 +0900

    ストロングパラメーターにnameとnicknameを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action :configure_permitted_parameters, if: :devise_controller?
         skip_before_action :verify_authenticity_token, if: :devise_controller?
+
+        protected
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname])
+                devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
+        end
 end

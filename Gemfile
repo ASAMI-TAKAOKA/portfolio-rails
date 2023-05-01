adcd03b71fe063bef3f9457294495ae984ce6fe6
Asami Takaoka <asamiyamabe61@gmail.com>
2023-05-01 21:25:51 +0900

    pry-railsの追加|account_updateのkeysにpasswordとemailを追加

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "devise"
 gem "devise_token_auth"
+gem "pry-rails"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
         before_action :configure_permitted_parameters, if: :devise_controller?
         skip_before_action :verify_authenticity_token, if: :devise_controller?
 
-        protected
+        private
         def configure_permitted_parameters
-                devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname])
-                devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :password, :email])
+                devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :password, :email])
         end
 end
